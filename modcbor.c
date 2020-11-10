6c77cb178c4681ba06d3187400a0488cead5dccc
Damiano Mazzella <damianomazzella@gmail.com>
2020-11-10 15:27:50 +0100

    Rimosso modulo python cbor. Rinominato modulo c da _cbor a cbor e aggiunti i metodi alias decode e encode

--- modcbor.c
+++ modcbor.c
@@ -489,6 +489,8 @@ STATIC const mp_rom_map_elem_t mp_module_ucbor_globals_table[] = {
     {MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR__cbor)},
     {MP_ROM_QSTR(MP_QSTR_loads), MP_ROM_PTR(&cbor_loads_obj)},
     {MP_ROM_QSTR(MP_QSTR_dumps), MP_ROM_PTR(&cbor_dumps_obj)},
+    {MP_ROM_QSTR(MP_QSTR_decode), MP_ROM_PTR(&cbor_loads_obj)},
+    {MP_ROM_QSTR(MP_QSTR_encode), MP_ROM_PTR(&cbor_dumps_obj)},
 };
 
 STATIC MP_DEFINE_CONST_DICT(mp_module_ucbor_globals, mp_module_ucbor_globals_table);
@@ -499,6 +501,6 @@ const mp_obj_module_t mp_module_ucbor = {
 };
 
 // Register the module to make it available in Python
-MP_REGISTER_MODULE(MP_QSTR__cbor, mp_module_ucbor, MICROPY_PY_UCBOR);
+MP_REGISTER_MODULE(MP_QSTR_cbor, mp_module_ucbor, MICROPY_PY_UCBOR);
 
 #endif // MICROPY_PY_UCBOR
