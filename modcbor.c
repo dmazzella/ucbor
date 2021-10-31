f4f844c75d8d84e8a5c0194331b245b30a279e98
Damiano Mazzella <damianomazzella@gmail.com>
2021-10-31 14:20:56 +0100

    Ottimizzazione di memoria

--- modcbor.c
+++ modcbor.c
@@ -47,25 +47,11 @@ STATIC mp_obj_t cbor_load_int(const byte ai, vstr_t *data_vstr)
     {
         val = mp_obj_new_int(ai);
     }
-    else if (ai == 24)
+    else if (ai >= 24 && ai <= 27)
     {
-        val = mp_obj_int_from_bytes_impl(true, 1, (const byte *)data_vstr->buf);
-        vstr_cut_head_bytes(data_vstr, 1);
-    }
-    else if (ai == 25)
-    {
-        val = mp_obj_int_from_bytes_impl(true, 2, (const byte *)data_vstr->buf);
-        vstr_cut_head_bytes(data_vstr, 2);
-    }
-    else if (ai == 26)
-    {
-        val = mp_obj_int_from_bytes_impl(true, 4, (const byte *)data_vstr->buf);
-        vstr_cut_head_bytes(data_vstr, 4);
-    }
-    else if (ai == 27)
-    {
-        val = mp_obj_int_from_bytes_impl(true, 8, (const byte *)data_vstr->buf);
-        vstr_cut_head_bytes(data_vstr, 8);
+        uint8_t n_bytes = (1 << (ai - 24));
+        val = mp_obj_int_from_bytes_impl(true, n_bytes, (const byte *)data_vstr->buf);
+        vstr_cut_head_bytes(data_vstr, n_bytes);
     }
 
     if (!mp_obj_is_int(val))
@@ -117,7 +103,7 @@ STATIC mp_obj_t cbor_load_dict(const byte ai, vstr_t *data_vstr)
     for (size_t i = 0; i < load_int_len; i++)
     {
         mp_obj_t key = cbor_loads(data_vstr);
-        mp_obj_t value = MP_OBJ_TO_PTR(cbor_loads(data_vstr));
+        mp_obj_t value = cbor_loads(data_vstr);
         mp_obj_dict_store(dict, key, value);
     }
     return dict;
@@ -210,7 +196,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(cbor_sort_key_obj, cbor_sort_key);
 
 STATIC mp_obj_t cbor_encode(mp_obj_t obj_data);
 
-STATIC vstr_t *cbor_dump_int(mp_obj_t obj_data, mp_int_t mt)
+STATIC void cbor_dump_int(mp_obj_t obj_data, mp_int_t mt, vstr_t *data_vstr)
 {
     mp_int_t data = mp_obj_get_int(obj_data);
     if (data < 0)
@@ -220,239 +206,181 @@ STATIC vstr_t *cbor_dump_int(mp_obj_t obj_data, mp_int_t mt)
     }
 
     mt = mt << 5;
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
+    if (data <= 23)
     {
-        if (data <= 23)
-        {
-            vstr_add_byte(vstr, (byte)(mt | data));
-        }
-        else if (data <= 0xff)
-        {
-            vstr_add_byte(vstr, (byte)(mt | 24));
-            vstr_add_byte(vstr, (byte)(data));
-        }
-        else if (data <= 0xffff)
-        {
-            vstr_add_byte(vstr, (byte)(mt | 25));
-            vstr_add_byte(vstr, (byte)((data >> 8) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 0) & 0xff));
-        }
+        vstr_add_byte(data_vstr, (byte)(mt | data));
+    }
+    else if (data <= 0xff)
+    {
+        vstr_add_byte(data_vstr, (byte)(mt | 24));
+        vstr_add_byte(data_vstr, (byte)(data));
+    }
+    else if (data <= 0xffff)
+    {
+        vstr_add_byte(data_vstr, (byte)(mt | 25));
+        vstr_add_byte(data_vstr, (byte)((data >> 8) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 0) & 0xff));
+    }
 #if UINT32_MAX >= 0xffffffff
-        else if (data <= 0xffffffff)
-        {
-            vstr_add_byte(vstr, (byte)(mt | 26));
-            vstr_add_byte(vstr, (byte)((data >> 24) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 16) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 8) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 0) & 0xff));
-        }
+    else if (data <= 0xffffffff)
+    {
+        vstr_add_byte(data_vstr, (byte)(mt | 26));
+        vstr_add_byte(data_vstr, (byte)((data >> 24) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 16) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 8) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 0) & 0xff));
+    }
 #if UINT32_MAX > 0xffffffff
-        else
-        {
-            vstr_add_byte(vstr, (byte)(mt | 27));
-            vstr_add_byte(vstr, (byte)((data >> 56) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 48) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 40) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 32) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 24) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 16) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 8) & 0xff));
-            vstr_add_byte(vstr, (byte)((data >> 0) & 0xff));
-        }
+    else
+    {
+        vstr_add_byte(data_vstr, (byte)(mt | 27));
+        vstr_add_byte(data_vstr, (byte)((data >> 56) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 48) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 40) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 32) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 24) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 16) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 8) & 0xff));
+        vstr_add_byte(data_vstr, (byte)((data >> 0) & 0xff));
+    }
 #endif
 #endif
-    }
-    return vstr;
 }
 
-STATIC vstr_t *cbor_dump_bool(mp_obj_t obj_data)
+STATIC void cbor_dump_bool(mp_obj_t obj_data, vstr_t *data_vstr)
 {
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
-    {
-        vstr_add_byte(vstr, (byte)(mp_obj_get_int(obj_data) ? 0xf5 : 0xf4));
-    }
-    return vstr;
+
+    vstr_add_byte(data_vstr, (byte)(mp_obj_get_int(obj_data) ? 0xf5 : 0xf4));
 }
 
-STATIC vstr_t *cbor_dump_text(mp_obj_t obj_data)
+STATIC void cbor_dump_text(mp_obj_t obj_data, vstr_t *data_vstr)
 {
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
-    {
-        size_t len = 0;
-        const char *text = mp_obj_str_get_data(obj_data, &len);
-
-        vstr_t *vstr_type = cbor_dump_int(mp_obj_new_int(len), 3);
-        if (vstr_type != NULL)
-        {
-            vstr_add_strn(vstr, vstr_type->buf, vstr_type->len);
-            vstr_free(vstr_type);
-        }
-
-        vstr_add_strn(vstr, text, len);
-    }
-    return vstr;
+    size_t len = 0;
+    const char *text = mp_obj_str_get_data(obj_data, &len);
+    cbor_dump_int(mp_obj_new_int(len), 3, data_vstr);
+    vstr_add_strn(data_vstr, text, len);
 }
 
-STATIC vstr_t *cbor_dump_bytes(mp_obj_t obj_data)
+STATIC void cbor_dump_bytes(mp_obj_t obj_data, vstr_t *data_vstr)
 {
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
+    mp_buffer_info_t bufinfo;
+    mp_get_buffer_raise(obj_data, &bufinfo, MP_BUFFER_READ);
+    cbor_dump_int(mp_obj_new_int(bufinfo.len), 2, data_vstr);
+    vstr_add_strn(data_vstr, (const char *)bufinfo.buf, bufinfo.len);
+}
+
+STATIC void cbor_dump_list(mp_obj_t obj_data, vstr_t *data_vstr)
+{
+    size_t len;
+    mp_obj_t *items;
+    mp_obj_get_array(obj_data, &len, &items);
+    cbor_dump_int(mp_obj_new_int(len), 4, data_vstr);
+
+    for (size_t i = 0; i < len; i++)
     {
         mp_buffer_info_t bufinfo;
-        mp_get_buffer_raise(obj_data, &bufinfo, MP_BUFFER_READ);
-
-        vstr_t *vstr_type = cbor_dump_int(mp_obj_new_int(bufinfo.len), 2);
-        if (vstr_type != NULL)
-        {
-            vstr_add_strn(vstr, vstr_type->buf, vstr_type->len);
-            vstr_free(vstr_type);
-        }
-
-        vstr_add_strn(vstr, (const char *)bufinfo.buf, bufinfo.len);
+        mp_get_buffer_raise(cbor_encode(items[i]), &bufinfo, MP_BUFFER_READ);
+        vstr_add_strn(data_vstr, (const char *)bufinfo.buf, bufinfo.len);
     }
-    return vstr;
 }
 
-STATIC vstr_t *cbor_dump_list(mp_obj_t obj_data)
+STATIC void cbor_dump_dict(mp_obj_t obj_data, vstr_t *data_vstr)
 {
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
-    {
-        size_t len;
-        mp_obj_t *items;
-        mp_obj_get_array(obj_data, &len, &items);
-        vstr_t *vstr_type = cbor_dump_int(mp_obj_new_int(len), 4);
-        if (vstr_type != NULL)
-        {
-            vstr_add_strn(vstr, vstr_type->buf, vstr_type->len);
-            vstr_free(vstr_type);
-        }
-
-        for (size_t i = 0; i < len; i++)
-        {
-            mp_buffer_info_t bufinfo;
-            mp_get_buffer_raise(cbor_encode(items[i]), &bufinfo, MP_BUFFER_READ);
-            vstr_add_strn(vstr, (const char *)bufinfo.buf, bufinfo.len);
-        }
-    }
-    return vstr;
-}
-
-STATIC vstr_t *cbor_dump_dict(mp_obj_t obj_data)
-{
-    vstr_t *vstr = NULL;
-    if ((vstr = vstr_new(16)) != NULL)
-    {
-        mp_map_t *map = mp_obj_dict_get_map(obj_data);
-        vstr_t *vstr_type = cbor_dump_int(mp_obj_new_int(map->used), 5);
-        if (vstr_type != NULL)
-        {
-            vstr_add_strn(vstr, vstr_type->buf, vstr_type->len);
-            vstr_free(vstr_type);
-        }
+    mp_map_t *map = mp_obj_dict_get_map(obj_data);
+    cbor_dump_int(mp_obj_new_int(map->used), 5, data_vstr);
 
 #if defined(MICROPY_PY_UCBOR_CANONICAL)
-        mp_obj_t items = mp_obj_new_list(0, NULL);
-        for (size_t i = 0; i < map->alloc; i++)
+    mp_obj_t items = mp_obj_new_list(0, NULL);
+    for (size_t i = 0; i < map->alloc; i++)
+    {
+        if (mp_map_slot_is_filled(map, i))
         {
-            if (mp_map_slot_is_filled(map, i))
-            {
-                mp_buffer_info_t bufinfo_key;
-                mp_get_buffer_raise(cbor_encode(map->table[i].key), &bufinfo_key, MP_BUFFER_READ);
-
-                mp_buffer_info_t bufinfo_value;
-                mp_get_buffer_raise(cbor_encode(map->table[i].value), &bufinfo_value, MP_BUFFER_READ);
-
-                mp_obj_t items_items[2] = {
-                    mp_obj_new_bytes(bufinfo_key.buf, bufinfo_key.len),
-                    mp_obj_new_bytes(bufinfo_value.buf, bufinfo_value.len)};
-                mp_obj_list_append(items, mp_obj_new_tuple(MP_ARRAY_SIZE(items_items), items_items));
-            }
-        }
-        mp_obj_t items_kwargs = mp_obj_new_dict(0);
-        mp_obj_dict_store(items_kwargs, MP_ROM_QSTR(MP_QSTR_key), MP_OBJ_FROM_PTR(&cbor_sort_key_obj));
-        mp_obj_list_sort(1, &items, mp_obj_dict_get_map(items_kwargs));
-
-        size_t array_len;
-        mp_obj_t *array_items;
-        mp_obj_get_array(items, &array_len, &array_items);
-        for (size_t i = 0; i < array_len; i++)
-        {
-            mp_obj_tuple_t *array_items_tuple = MP_OBJ_TO_PTR(array_items[i]);
-
             mp_buffer_info_t bufinfo_key;
-            mp_get_buffer_raise(array_items_tuple->items[0], &bufinfo_key, MP_BUFFER_READ);
-            vstr_add_strn(vstr, (const char *)bufinfo_key.buf, bufinfo_key.len);
+            mp_get_buffer_raise(cbor_encode(map->table[i].key), &bufinfo_key, MP_BUFFER_READ);
 
             mp_buffer_info_t bufinfo_value;
-            mp_get_buffer_raise(array_items_tuple->items[1], &bufinfo_value, MP_BUFFER_READ);
-            vstr_add_strn(vstr, (const char *)bufinfo_value.buf, bufinfo_value.len);
-        }
-#else
-        for (size_t i = 0; i < map->alloc; i++)
-        {
-            if (mp_map_slot_is_filled(map, i))
-            {
-                mp_buffer_info_t bufinfo_key;
-                mp_get_buffer_raise(cbor_encode(map->table[i].key), &bufinfo_key, MP_BUFFER_READ);
-                vstr_add_strn(vstr, (const char *)bufinfo_key.buf, bufinfo_key.len);
+            mp_get_buffer_raise(cbor_encode(map->table[i].value), &bufinfo_value, MP_BUFFER_READ);
 
-                mp_buffer_info_t bufinfo_value;
-                mp_get_buffer_raise(cbor_encode(map->table[i].value), &bufinfo_value, MP_BUFFER_READ);
-                vstr_add_strn(vstr, (const char *)bufinfo_value.buf, bufinfo_value.len);
-            }
+            mp_obj_t items_items[2] = {
+                mp_obj_new_bytes(bufinfo_key.buf, bufinfo_key.len),
+                mp_obj_new_bytes(bufinfo_value.buf, bufinfo_value.len)};
+            mp_obj_list_append(items, mp_obj_new_tuple(MP_ARRAY_SIZE(items_items), items_items));
         }
-#endif
     }
-    return vstr;
+    mp_obj_t items_kwargs = mp_obj_new_dict(0);
+    mp_obj_dict_store(items_kwargs, MP_ROM_QSTR(MP_QSTR_key), MP_OBJ_FROM_PTR(&cbor_sort_key_obj));
+    mp_obj_list_sort(1, &items, mp_obj_dict_get_map(items_kwargs));
+
+    size_t array_len;
+    mp_obj_t *array_items;
+    mp_obj_get_array(items, &array_len, &array_items);
+    for (size_t i = 0; i < array_len; i++)
+    {
+        mp_obj_tuple_t *array_items_tuple = MP_OBJ_TO_PTR(array_items[i]);
+
+        mp_buffer_info_t bufinfo_key;
+        mp_get_buffer_raise(array_items_tuple->items[0], &bufinfo_key, MP_BUFFER_READ);
+        vstr_add_strn(data_vstr, (const char *)bufinfo_key.buf, bufinfo_key.len);
+
+        mp_buffer_info_t bufinfo_value;
+        mp_get_buffer_raise(array_items_tuple->items[1], &bufinfo_value, MP_BUFFER_READ);
+        vstr_add_strn(data_vstr, (const char *)bufinfo_value.buf, bufinfo_value.len);
+    }
+#else
+    for (size_t i = 0; i < map->alloc; i++)
+    {
+        if (mp_map_slot_is_filled(map, i))
+        {
+            mp_buffer_info_t bufinfo_key;
+            mp_get_buffer_raise(cbor_encode(map->table[i].key), &bufinfo_key, MP_BUFFER_READ);
+            vstr_add_strn(data_vstr, (const char *)bufinfo_key.buf, bufinfo_key.len);
+
+            mp_buffer_info_t bufinfo_value;
+            mp_get_buffer_raise(cbor_encode(map->table[i].value), &bufinfo_value, MP_BUFFER_READ);
+            vstr_add_strn(data_vstr, (const char *)bufinfo_value.buf, bufinfo_value.len);
+        }
+    }
+#endif
 }
 
 STATIC mp_obj_t cbor_encode(mp_obj_t obj_data)
 {
-    vstr_t result_vstr;
-    vstr_init(&result_vstr, 16);
     const mp_obj_type_t *obj_data_type = mp_obj_get_type(obj_data);
+    vstr_t data_vstr;
+    vstr_init(&data_vstr, 16);
 
-    vstr_t *vstr = NULL;
     if (obj_data_type == &mp_type_int)
     {
-        vstr = cbor_dump_int(obj_data, 0);
+        cbor_dump_int(obj_data, 0, &data_vstr);
     }
     else if (obj_data_type == &mp_type_bool)
     {
-        vstr = cbor_dump_bool(obj_data);
+        cbor_dump_bool(obj_data, &data_vstr);
     }
     else if (obj_data_type == &mp_type_str)
     {
-        vstr = cbor_dump_text(obj_data);
+        cbor_dump_text(obj_data, &data_vstr);
     }
     else if (obj_data_type == &mp_type_bytes || obj_data_type == &mp_type_bytearray || obj_data_type == &mp_type_memoryview)
     {
-        vstr = cbor_dump_bytes(obj_data);
+        cbor_dump_bytes(obj_data, &data_vstr);
     }
     else if (obj_data_type == &mp_type_list || obj_data_type == &mp_type_tuple)
     {
-        vstr = cbor_dump_list(obj_data);
+        cbor_dump_list(obj_data, &data_vstr);
     }
     else if (obj_data_type == &mp_type_dict)
     {
-        vstr = cbor_dump_dict(obj_data);
+        cbor_dump_dict(obj_data, &data_vstr);
     }
     else
     {
         nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("Unsupported value: %s"), mp_obj_get_type_str(obj_data)));
     }
 
-    if (vstr != NULL)
-    {
-        vstr_add_strn(&result_vstr, vstr->buf, vstr->len);
-        vstr_free(vstr);
-    }
-
-    return mp_obj_new_bytes((byte *)result_vstr.buf, result_vstr.len);
+    mp_obj_t val = mp_obj_new_bytes((byte *)data_vstr.buf, data_vstr.len);
+    vstr_clear(&data_vstr);
+    return val;
 }
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(cbor_encode_obj, cbor_encode);
